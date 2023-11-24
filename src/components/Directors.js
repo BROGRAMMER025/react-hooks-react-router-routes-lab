272def99a1e4c40fcea19bac3cbbc5acff01a24c
Allan Kinyua Murigi <allan.murigi@student.moringaschool.com>
2023-11-24 14:43:02 +0300

    Done with assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,23 @@
-import React from "react";
-import { directors } from "../data";
 
-function Directors() {
-  return <div>{/*{code here}*/}</div>;
+import React from 'react';
+import data from '../data';
+
+const Directors = () => {
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {data.directors.map(director => (
+        <div key={director.id}>
+          <h2>{director.name}</h2>
+          <ul>
+            {director.movies.map(movie => (
+              <li key={movie}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Directors;
